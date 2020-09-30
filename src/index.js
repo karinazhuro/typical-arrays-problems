b874946f3f801d5a4086ba9fdf12907cc9524c59
karinazhuro <44468931+karinazhuro@users.noreply.github.com>
2020-09-30 15:59:32 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'object' || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'object' || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum = 0;
+
+    if (typeof array !== 'object' || array.length === 0) {
+        return 0;
+    } else {
+        for (let number of array) {
+            sum += number;
+        }
+        return sum / array.length;
+    }
 }
